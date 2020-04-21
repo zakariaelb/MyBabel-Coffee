537f6f54052d9fe5721ed8b14884264c7f52c848
zelbatmi <zakari_a@icloud.com>
2020-04-21 16:53:31 +0000

    v0.1

--- app/src/main/java/digiplus/ma/mybabelcoffee/MainActivity.java
+++ app/src/main/java/digiplus/ma/mybabelcoffee/MainActivity.java
@@ -9,41 +9,54 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
-int quantity = 0;
+    int quantity = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-        public void submitOrder(View view){
-            EditText NameField = (EditText)findViewById(R.id.NameField);
-            String NameValue = NameField.getText().toString();
-            CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-            boolean HasWhippedCream = whippedCreamCheckBox.isChecked();
-            CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
-            boolean HasChocolate = chocolateCheckBox.isChecked();
-            int price = calculatePrice(HasWhippedCream, HasChocolate);
-            String message = createOrderSummary(NameValue, price, HasWhippedCream, HasChocolate);
-            displayMessage(message);
+
+    public void submitOrder(View view) {
+        EditText NameField = (EditText) findViewById(R.id.NameField);
+        String NameValue = NameField.getText().toString();
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean HasWhippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean HasChocolate = chocolateCheckBox.isChecked();
+        int price = calculatePrice(HasWhippedCream, HasChocolate);
+        String message = createOrderSummary(NameValue, price, HasWhippedCream, HasChocolate);
+        displayMessage(message);
     }
-        public void Decrement(View view) {
-            quantity = quantity - 1;
-            displayQuantity(quantity);
+    public void Decrement(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity - 1;
+        displayQuantity(quantity);
     }
-        public void Increment(View view) {
-            quantity = quantity + 1;
-            displayQuantity(quantity);
+    public void Increment(View view) {
+        if (quantity == 4) {
+            Toast.makeText(this, "You cannot have more than 4 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity + 1;
+        displayQuantity(quantity);
     }
-        private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
-            int basePrice = 5;
-            if (addWhippedCream) {
-                basePrice = basePrice + 1;
-            }
-            if (addChocolate) {
-                basePrice = basePrice + 2;
-            }
-            return quantity * basePrice;
+
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int basePrice = 5;
+        if (addWhippedCream) {
+            basePrice = basePrice + 1;
+        }
+        if (addChocolate) {
+            basePrice = basePrice + 2;
+        }
+        return quantity * basePrice;
     }
 
 
@@ -51,17 +64,21 @@ int quantity = 0;
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + NumberOfCoffees);
     }
+
     private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
     }
-    /** *********************************************** */
+
+    /**
+     * **********************************************
+     */
     private String createOrderSummary(String NameValue, int price, boolean addWhippedCream, boolean addChocolate) {
 
         String priceMessage = ("Name : " + NameValue);
-        priceMessage += "\n Add whipped Cream ? : " +addWhippedCream;
-        priceMessage += "\n Add chocolate ? : " +addChocolate;
-        priceMessage += "\n Quantity : " +quantity;
+        priceMessage += "\n Add whipped Cream ? : " + addWhippedCream;
+        priceMessage += "\n Add chocolate ? : " + addChocolate;
+        priceMessage += "\n Quantity : " + quantity;
         priceMessage += "\n Total : $" + price;
         priceMessage += "\n Thank you !";
         return priceMessage;
